45b707d4e3222970444b8b16b407078f36e19386
bryantran21 <bryantran21@gmail.com>
2026-04-20 13:18:02 -0500

    corrected import

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useState } from 'react'
-import { supabase } from '../lib/supabase' // Use that relative path that worked earlier
-
+import { supabase } from '../../lib/supabase'
 export default function LoginPage() {
   const [email, setEmail] = useState('')
   const [loading, setLoading] = useState(false)
